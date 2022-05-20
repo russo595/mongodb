2bf7a42ba73aafd3dc85f42842f06b32f9de4e7c
Rustem Sabitov <russab92@mail.ru>
2022-05-21 00:59:20 +0300

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.spring") version "1.6.10"
+    id("de.gliderpilot.semantic-release") version "1.4.2"
 }
 
 group = "com.example"
